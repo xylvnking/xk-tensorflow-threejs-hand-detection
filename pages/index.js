34bc9a00acccf79852636b6307cd67910ab49f8a
Dylan King <xylvnking@gmail.com>
2022-09-22 15:41:08 -0400

    moved webcam check to only happen on load and used ref to check it. want to useFrame to drive the threejs position instead of state because in theory the component shouldnt need to rerender

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,7 @@ function Box(props) {
   // const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
   // useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  // useFrame((gl, delta) => (props.handRotation))
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
@@ -59,6 +60,9 @@ export default function Home() {
   const canvasRef = useRef(null)
   const [handData, setHandData] = useState(null)
   const handPositionRef = useRef([0,0,0])
+  const [webcamReady, setWebcamReady] = useState(false)
+  const webcamReadyRef = useRef(false)
+  const videoRef = useRef(null)
 
   const [vectorFromHandData, setVectorFromHandData] = useState([0,0,0])
 
@@ -76,40 +80,46 @@ export default function Home() {
 
   console.log(handPositionRef.current)
 
+  
+
+
   const detectHand = async (net) => {
     if (
-      typeof webcamRef.current !=='undefined' && 
-      webcamRef.current !== null && 
-      webcamRef.current.video.readyState === 4
+      // typeof webcamRef.current !=='undefined' && 
+      // webcamRef.current !== null && 
+      // webcamRef.current.video.readyState === 4
+      webcamReadyRef.current == true
       ) {
-        const video = webcamRef.current.video
-        const videoWidth = webcamRef.current.video.videoWidth
-        const videoHeight = webcamRef.current.video.videoHeight
-        webcamRef.current.video.width = videoWidth
-        webcamRef.current.video.height = videoHeight
-        // canvasRef.current.width = videoWidth
-        // canvasRef.current.height = videoHeight
+        // console.log('webcam check passed')
+        // const video = webcamRef.current.video
+        // const videoWidth = webcamRef.current.video.videoWidth
+        // const videoHeight = webcamRef.current.video.videoHeight
+        // webcamRef.current.video.width = videoWidth
+        // webcamRef.current.video.height = videoHeight
 
-        const hand = await net.estimateHands(video)
+
+        const hand = await net.estimateHands(videoRef.current)
         if (hand[0]) {
+          console.log('hand in sight')
           // console.log(hand)
           handPositionRef.current = [
             (Math.round(hand[0].annotations.indexFinger[0][1]) / 50),
             0,
             (Math.round(hand[0].annotations.indexFinger[0][0]) / 100)
           ]
-          setHandData(hand[0])
-          setVectorFromHandData(
-            [
-              (Math.round(hand[0].annotations.indexFinger[0][1]) / 50),
-              0,
-              (Math.round(hand[0].annotations.indexFinger[0][0]) / 100)
-            ]
-          )
+          console.log(handPositionRef.current)
+          // setHandData(hand[0])
+          // setVectorFromHandData(
+          //   [
+          //     (Math.round(hand[0].annotations.indexFinger[0][1]) / 50),
+          //     0,
+          //     (Math.round(hand[0].annotations.indexFinger[0][0]) / 100)
+          //   ]
+          // )
 
 
         } else {
-          setHandData(null)
+          // setHandData(null)
         }
       }
   }
@@ -117,8 +127,34 @@ export default function Home() {
 
 
   useEffect(() => {
-    runHandPose()
+    console.log('webcamcheck useffect called')
+    const checkWebcam = () => {
+      if (
+        typeof webcamRef.current !=='undefined' 
+        && 
+        webcamRef.current !== null 
+        // && 
+        // webcamRef.current.video.readyState === 4
+        ) {
+          console.log('check webcam success')
+
+          const video = webcamRef.current.video
+          const videoWidth = webcamRef.current.video.videoWidth
+          const videoHeight = webcamRef.current.video.videoHeight
+          videoRef.current = video
+
+          webcamRef.current.video.width = videoWidth
+          webcamRef.current.video.height = videoHeight
+          // canvasRef.current.width = videoWidth
+          // canvasRef.current.height = videoHeight
+          setWebcamReady(true)
+          webcamReadyRef.current = true
+          runHandPose()
+        }
+    }
+    checkWebcam()
   }, [])
+  // }, [webcamRef.current])
 
   return (
     <div className={styles.container}>
@@ -167,19 +203,28 @@ export default function Home() {
           {/* <Suspense>
               <Model/>
           </Suspense> */}
+
           <ambientLight intensity={0.5} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
           <pointLight position={[-10, -10, -10]} />
+
           {/* <Box position={[-1.2, 0, 0]} /> */}
           <Box 
-            position={[0, 0, 0]} 
             // position={[
-            //   (vectorFromHandData[2]/ 5),
-            //   (vectorFromHandData[0]/ 5),
-            //   0,
-            // ]} 
-            // rotation={[1, 3, 50]}
-            // rotation={vectorFromHandData}
+            //     (handPositionRef.current[2]/ 5),
+            //     (handPositionRef.current[0]/ 5),
+            //     0,
+            //   ]} 
+            // position={[
+            //     (vectorFromHandData[2]/ 5),
+            //     (vectorFromHandData[0]/ 5),
+            //     0,
+            //   ]} 
+              // rotation={[1, 3, 50]}
+              // rotation={vectorFromHandData}
+              // handRotation={handPositionRef.current}
+              position={[0, 0, 0]} 
+            // position={handPositionRef.current}
             rotation={handPositionRef.current}
             // rotation={
             //   [
@@ -191,11 +236,11 @@ export default function Home() {
         </Canvas>
         </div>
         
-        {
+        {/* {
           handData &&
         // <h1>finger position is {typeof handData.annotations.indexFinger[0][0]}</h1>
         <h1>finger position is {Math.round(handData.annotations.indexFinger[0][0])}</h1>
-        }
+        } */}
 
     </div>
   )
